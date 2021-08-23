fbcc4382c44557fea658d74b44008ef858552c9d
ihashmatullah <hashnoorzai@yahoo.com>
2021-08-23 16:47:27 -0500

    Testing initial commit with Foo file

---
+++ src/main/java/guru/springframework/spring5webapp/Foo.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by HashmatN at 4:28 PM on Aug 23, 2021
+ */
+public class Foo {
+  public static void main(String[] args) {
+
+    System.out.println("This is a test git push");
+  }
+
+}
